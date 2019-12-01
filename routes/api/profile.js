a2ac0b4277ac3317a3c392144b4b295a6e0240f5
Nam <nam.t171846@vanlanguni.vn>
2019-12-01 17:05:36 +0700

    complete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,10 @@ const { check, validationResult } = require("express-validator");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
+//NOTE  GET SINGLE PROFILE BY ID BUT PRIVATE AND CHECK AUTH
+// @route   Get api/profile/me
+// @desc    Get current users profile
+// @access  Private
 router.get("/me", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({
@@ -19,8 +23,9 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-// @route   Get api/profile/me
-// @desc    Get current users profile
+//NOTE  ADD USERS PROFILE BUT PRIVATE
+// @route   Post api/profile/
+// @desc    ADD users profile
 // @access  Private
 router.post(
   "/",
@@ -54,7 +59,7 @@ router.post(
       instagram,
       linkedin
     } = req.body;
-    // Build profile object
+    //REVIEW  Build profile object
     const profileFields = {};
     profileFields.user = req.user.id;
     if (company) profileFields.company = company;
@@ -66,7 +71,7 @@ router.post(
     if (skills) {
       profileFields.skills = skills.split(",").map(skill => skill.trim());
     }
-    // Build socail object
+    //REVIEW  Build socail object
     profileFields.social = {};
     if (youtube) profileFields.social.youtube = youtube;
     if (twitter) profileFields.social.twitter = twitter;
@@ -95,6 +100,8 @@ router.post(
     }
   }
 );
+
+//NOTE GET ALL USER PROFILE
 // @route   Get api/profile/
 // @desc    Get all user profile
 // @access  Public
@@ -107,6 +114,7 @@ router.get("/", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+//NOTE  GET SINGLE USER PROFILE BY ID BUT PUBLIC
 // @route   Get api/profile/user/:user_id
 // @desc    Get profile by user ID
 // @access  Public
@@ -124,6 +132,7 @@ router.get("/user/:user_id", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+//NOTE DELETE PROFILE, USER AND POST BUT PRIVATE AND CHECK AUTH
 // @route   Delete api/profile/
 // @desc    Delete Prfilte , user & post
 // @access  Private
@@ -140,4 +149,156 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+//NOTE  ADD PROFILE EXPERIENCE BUT PRIVATE AND CHECK AUTH
+// @route   PUT api/profile/experience
+// @desc    Add Prfilte experience
+// @access  Private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "title is required")
+        .not()
+        .isEmpty(),
+      check("company", "company is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+//NOTE  DELETE PROFILE EXPERIENCE BUT PRIVATE AND CHECK AUTH
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    DELETE Prfilte experience
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//NOTE  ADD PROFILE EDUCATION BUT PRIVATE AND CHECK AUTH
+// @route   PUT api/profile/education
+// @desc    Add Prfilte education
+// @access  Private
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "school is required")
+        .not()
+        .isEmpty(),
+      check("degree", "degree is required")
+        .not()
+        .isEmpty(),
+      check("fieldofstudy", "fieldofstudy is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//NOTE  DELETE PROFILE EDUCATION BUT PRIVATE AND CHECK AUTH
+// @route   DELETE api/profile/education/:edu_id
+// @desc    DELETE Prfilte education
+// @access  Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
