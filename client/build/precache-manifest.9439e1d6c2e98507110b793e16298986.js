6400b3a296fdf8d19bcd4b87694a08c93dd94aac
Nam <nam.t171846@vanlanguni.vn>
2019-12-13 22:20:58 +0700

    deploy

---
+++ client/build/precache-manifest.9439e1d6c2e98507110b793e16298986.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769436d6b7fb5c4d22b8098615bbc5fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97aeaa23b357e0c927c5",
+    "url": "/static/css/main.8b42bec8.chunk.css"
+  },
+  {
+    "revision": "f23ea922927e894fbacc",
+    "url": "/static/js/2.070481b3.chunk.js"
+  },
+  {
+    "revision": "97aeaa23b357e0c927c5",
+    "url": "/static/js/main.f2a04cf8.chunk.js"
+  },
+  {
+    "revision": "18ceee0904d39392a2a3",
+    "url": "/static/js/runtime-main.8095ec4f.js"
+  },
+  {
+    "revision": "a7d4f323bb3f364fd871fbcc9e2f138b",
+    "url": "/static/media/bannerdoc.a7d4f323.jpg"
+  },
+  {
+    "revision": "37dd2b96b585e2a086cf5807dde9a07f",
+    "url": "/static/media/bannerdoc2.37dd2b96.jpg"
+  },
+  {
+    "revision": "65d6eec0278365637c84d2064c02150d",
+    "url": "/static/media/bannerdoc3.65d6eec0.gif"
+  },
+  {
+    "revision": "efd3dabed040c5f6e07a339d156b1bc0",
+    "url": "/static/media/bannerdoc4.efd3dabe.jpg"
+  },
+  {
+    "revision": "7372099d951cf43cb9861489361fa742",
+    "url": "/static/media/bike.7372099d.jpg"
+  },
+  {
+    "revision": "cc67c480fc3acbac18ebdaf80fd70f3c",
+    "url": "/static/media/dog.cc67c480.jpg"
+  },
+  {
+    "revision": "24f308020acdbecd87192475a06f18da",
+    "url": "/static/media/fashion.24f30802.jpg"
+  },
+  {
+    "revision": "2e82ecf120e3e8c4c100a96f724c7443",
+    "url": "/static/media/icon2.2e82ecf1.png"
+  },
+  {
+    "revision": "ae72d51b444d9d88b1e7657d0ca51d1b",
+    "url": "/static/media/job.ae72d51b.jpg"
+  },
+  {
+    "revision": "c370d7b0208bef34d8cf2d09f451fc09",
+    "url": "/static/media/member.c370d7b0.gif"
+  },
+  {
+    "revision": "a5e99fd526b4b9f9cb21863847a36df7",
+    "url": "/static/media/mobile.a5e99fd5.jpg"
+  },
+  {
+    "revision": "d935bcf8e25ae82d359b3a3e35b72abc",
+    "url": "/static/media/phone.d935bcf8.png"
+  },
+  {
+    "revision": "b8f11e003e035fc9885d23db408c27d4",
+    "url": "/static/media/quancao3.b8f11e00.jpg"
+  },
+  {
+    "revision": "6664dd9dc42d17369c9fe12428bde61e",
+    "url": "/static/media/quangcao.6664dd9d.jpg"
+  },
+  {
+    "revision": "e34ec19e7b03b1a06754ba65de111035",
+    "url": "/static/media/quangcao3.e34ec19e.jpg"
+  },
+  {
+    "revision": "7aac952f37b124bf168dd34f24577cee",
+    "url": "/static/media/quangcao4.7aac952f.gif"
+  },
+  {
+    "revision": "503a3fb405c811b3c914d0c83e85a430",
+    "url": "/static/media/quangcao5.503a3fb4.gif"
+  },
+  {
+    "revision": "e2796c2dd3ef7074c6e2ed38a018b9b5",
+    "url": "/static/media/quangcao6.e2796c2d.gif"
+  },
+  {
+    "revision": "5bb99b774525a8aad8189545a53585ed",
+    "url": "/static/media/service.5bb99b77.png"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "2c0c5093f8c8575e7dccc10c26032456",
+    "url": "/static/media/spinner.2c0c5093.gif"
+  }
+]);
